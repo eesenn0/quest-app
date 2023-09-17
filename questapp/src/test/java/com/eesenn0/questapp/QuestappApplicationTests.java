26931c9184fca9b9775cd63ef33383aa64a7c87e
eesenn0 <emreesen7@gmail.com>
2023-09-17 14:27:25 +0300

    connect mysql to db

---
+++ questapp/src/test/java/com/eesenn0/questapp/QuestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eesenn0.questapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
