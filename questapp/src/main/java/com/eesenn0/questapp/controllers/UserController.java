407168d64af732cbd3dfb8e40a3c9a9374b4afe5
eesenn0 <emreesen7@gmail.com>
2023-09-18 12:55:21 +0300

    connection between the entity classes

--- questapp/src/main/java/com/eesenn0/questapp/controllers/UserController.java
+++ questapp/src/main/java/com/eesenn0/questapp/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.eesenn0.questapp.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +39,24 @@ public class UserController {
     public User getOneUser(@PathVariable Long userId) {
         // custom exception
         return userRepository.findById(userId).orElse(null);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateOneUser(@PathVariable Long userId, @RequestBody User newUser) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isPresent()) {
+            User foundUser = user.get();
+            foundUser.setUsername(newUser.getUsername());
+            foundUser.setPassword(newUser.getPassword());
+            userRepository.save(foundUser);
+            return foundUser;
+        } else
+            return null;
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteOneUser(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
     }
 }
--- questapp/src/main/java/com/eesenn0/questapp/entities/Like.java
+++ questapp/src/main/java/com/eesenn0/questapp/entities/Like.java
@@ -1,17 +1,35 @@
 package com.eesenn0.questapp.entities;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name="p_like")
+@Table(name = "p_like")
 @Data
 public class Like {
-    
+
     @Id
     Long id;
-    Long postId;
-    Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
 }
