2a3e3abb9567a78f3ba85137e1b88c1bfed66c8b
eesenn0 <emreesen7@gmail.com>
2023-09-19 16:06:19 +0300

    created post class for react

---
+++ quest-app/src/components/Post/Post.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
+
+function Post() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [postList, setPostList] = useState([]);
+}
